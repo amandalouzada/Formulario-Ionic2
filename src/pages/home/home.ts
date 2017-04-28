20b6bd14d584276dd5f95c4343237fd7eddd806b
Amanda Louzada <amandanuneslouzada@gmail.com>
2017-04-28 18:02:14 -0300

    Add page formulário

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { FormularioPage } from '../formulario/formulario'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  formPage:any = FormularioPage;
   constructor(public navCtrl: NavController) {
 
+  }
+
+
+  goForm(){
+    this.navCtrl.push(this.formPage);
   }
 
 }
